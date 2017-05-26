65c48beebf14982b130cfba1b55a8f9d6e4a3a73
LeoVergara1 <leovergara.dark@gmail.com>
2017-05-25 22:18:57 -0500

    adding template

--- app/initialize.js
+++ app/initialize.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
-  require('js/classie.js');
-  require('js/bootstrap.min.js');
-
-   require( "js/bootstrap.min.js")
-   require( "js/tooltip.js")
-   require( "js/bootstrap-extensions/bootstrap-dropdownhover.js")
-   require( "js/bootstrap-extensions/bootstrap.slide-menu.js")
-   require( "js/bootstrap-extensions/bootstrap-validator.js")
-   require( "js/modernizr.custom.js")
-   require( "js/jquery.mousewheel-3.0.6.pack.js")
-   require( "js/jquery.easing.js")
-   require( "js/classie.js")
-
-   require("js/parallax.min.js")
-   require("js/jquery.ui.totop.min.js")
-   require("js/waypoints.min.js")
-
-   require("js/sidebar-transitions/sidebar-transitions.js")
-   require("js/jquery.smoothscroll.js")
-   //require("js/jquery.fancybox.pack.js?v=2.1.5")
-   require("js/jquery.countTo.js")
-   require("js/jquery.easypiechart.min.js")
-   require("js/owl.carousel.min.js")
-   require("js/html5shiv.js")
-   require("js/respond.min.js")
-  //  <script type="text/javascript" src="js/dvs.ga.js"></script>
-
-
-
-
-   // <!-- Revolutions slider -->
- require("js/jquery.themepunch.plugins.min.js")
- require("js/jquery.themepunch.revolution.min.js")
+//  require('js/classie.js');
+//  require('js/bootstrap.min.js');
+//
+//   require( "js/bootstrap.min.js")
+//   require( "js/tooltip.js")
+//   require( "js/bootstrap-extensions/bootstrap-dropdownhover.js")
+//   require( "js/bootstrap-extensions/bootstrap.slide-menu.js")
+//   require( "js/bootstrap-extensions/bootstrap-validator.js")
+// //  require( "js/modernizr.custom.js")
+//   require( "js/jquery.mousewheel-3.0.6.pack.js")
+//   require( "js/jquery.easing.js")
+//   require( "js/classie.js")
+//
+//   require("js/parallax.min.js")
+//  require("js/jquery.ui.totop.min.js")
+// //  require("js/waypoints.min.js")
+//
+////   require("js/sidebar-transitions/sidebar-transitions.js")
+//   require("js/jquery.smoothscroll.js")
+//   //require("js/jquery.fancybox.pack.js?v=2.1.5")
+//  // require("js/jquery.countTo.js")
+//  // require("js/jquery.easypiechart.min.js")
+//  // require("js/owl.carousel.min.js")
+// //  require("js/html5shiv.js")
+//   require("js/respond.min.js")
+//  //  <script type="text/javascript" src="js/dvs.ga.js"></script>
+//
+//
+//
+//
+//   // <!-- Revolutions slider -->
+//// require("js/jquery.themepunch.plugins.min.js")
+// require("js/jquery.themepunch.revolution.js")
 });
