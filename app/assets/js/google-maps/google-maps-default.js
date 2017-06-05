318f5d98e2d8119bede006efaf81cc5c8622b98a
LeoVergara1 <leovergara.dark@gmail.com>
2017-06-04 19:34:47 -0500

    changes generals

--- app/assets/js/google-maps/google-maps-default.js
+++ app/assets/js/google-maps/google-maps-default.js
@@ -1,7 +1,10 @@
 var map;
+var x=51.5255069;
+var y=-0.1834507;
+
 
 function initialize() {
-  var myLatlng = new google.maps.LatLng(51.5255069,  -0.0836207);
+  var myLatlng = new google.maps.LatLng(x,  y);
   var mapOptions = {
     zoom: 14,
 	  scrollwheel: false,
@@ -16,15 +19,18 @@ function initialize() {
 	  animation: google.maps.Animation.DROP,
       title: 'Hello World!'
   });
-  
+
   var contentString = '<div class="info-window-content"><h2>Web Pixels</h2>'+
   					  '<h3>Designing forward</h3>'+
 					  '<p>Some more details for directions or company informations...</p></div>';
-					  
+
   var infowindow = new google.maps.InfoWindow({
       content: contentString
   });
-  
+
+  console.log("Estoy aaa");
+  $("body").on('click',"#contact1", function (e){ x=60; y=0.1; console.log("presiono"); initialize();  } )
+
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(map,marker);
   });
@@ -32,6 +38,14 @@ function initialize() {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+function reloadMap() {
+
+ $("body").on('click',"#contact1", function (e){ console.log("Como estas");  } )
+ $("body").on('click',".col-md-4", function (e){ console.log("Como estas");  } )
+
+}
+
 $('a[data-type="gmap"]').on('shown.bs.tab', function (e) {
+  reloadMap();
   initialize();
 })
